97e949e59a78f93f361a327d22f5f884656dff8d
Nirzhor <rabbi.cse1.bu@gmail.com>
2021-01-04 23:17:48 +0600

    Updated v3 (Connect-me) -> Poster

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCMBkYXsUDNENza-IjHb29-6a1bv7XBFIw",
-    authDomain: "react-connect-me.firebaseapp.com",
-    projectId: "react-connect-me",
-    storageBucket: "react-connect-me.appspot.com",
-    messagingSenderId: "207673487691",
-    appId: "1:207673487691:web:53e3cddaf48a4a2737b8ce"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
